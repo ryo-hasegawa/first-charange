8fc4bc405763dcfb24d406347bbb09043884f771
ryo.hasegawa <HaseMac@HaseMac-Mk3.local>
2018-11-18 00:14:24 +0900

    ハンバーガーメニューの実装

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // ここからコードを書く
 $(function(){
   $("#text").fadeIn(4000);
+
+  $('span').click(function(){
+    if($('.mobile-menu-wrapper').hasClass('open')){
+      $('.mobile-menu-wrapper').removeClass('open');
+      $('.mobile-menu-wrapper').slideUp(300);
+      $('header').css('background-color','rgba(239, 239, 239, 0.28)');
+      $(this).html('<i class="fas fa-bars"></i>');
+    }else {
+      $('.mobile-menu-wrapper').addClass('open');
+      $('.mobile-menu-wrapper').slideDown(300);
+      $('header').css('background-color','rgba(0, 0, 0, 0.7)');
+      $(this).html('<i class="fas fa-times"></i>');
+
+    };
+  });
 });
