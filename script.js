f5fa9af442616f1402e93db6f9bb382991dae4f3
ryo.hasegawa <HaseMac@HaseMac-Mk3.local>
2018-11-19 19:11:52 +0900

    ロゴクリックでindex.htmlに移動 designページ追加

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(function(){
     if($('.mobile-menu-wrapper').hasClass('open')){
       $('.mobile-menu-wrapper').removeClass('open');
       $('.mobile-menu-wrapper').slideUp(300);
-      $('header').css('background-color','rgba(239, 239, 239, 0.28)');
+      $('header').css('background-color','rgba(31, 31, 31, 0.45)');
       $(this).html('<i class="fas fa-bars"></i>');
     }else {
       $('.mobile-menu-wrapper').addClass('open');
