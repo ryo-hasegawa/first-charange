11b848a8756474a2aa5386a2d49d7fedb8188d23
ryo.hasegawa <HaseMac@HaseMac-Mk3.local>
2018-11-21 17:17:39 +0900

    Worksページ作成。レスポンシブ未対応

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ $(function(){
     if($('.mobile-menu-wrapper').hasClass('open')){
       $('.mobile-menu-wrapper').removeClass('open');
       $('.mobile-menu-wrapper').slideUp(300);
-      $('header').css('background-color','rgba(31, 31, 31, 0.45)');
       $(this).html('<i class="fas fa-bars"></i>');
     }else {
       $('.mobile-menu-wrapper').addClass('open');
       $('.mobile-menu-wrapper').slideDown(300);
-      $('header').css('background-color','rgba(0, 0, 0, 0.7)');
       $(this).html('<i class="fas fa-times"></i>');
 
     };
